0cd36853dfaa31e4c83d3cc2a9c6fde663ed4837
Seth Kingsley <sethk@sethk-mbpro.local>
2013-09-28 15:02:00 -0700

    etc. etc.

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -3,19 +3,27 @@ using System.Collections;
 
 public class PlayerControl : MonoBehaviour
 {
+	GameObject airplane;
 
 	// Use this for initialization
 	void Start ()
 	{
-	
+		// If we are starting in the editor from the simulation scene, load the editor first
+		if (!Core.SimulationMode)
+			Application.LoadLevel(0);
+		else
+		{
+			GameObject spawnPoint = GameObject.Find("Spawn Point");
+			airplane = GameObject.Find("Airplane");
+			airplane.rigidbody.useGravity = true;
+			airplane.transform.position = spawnPoint.transform.position;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		Camera.main.transform.LookAt(transform);
-		rigidbody.useGravity = true;
-		rigidbody.AddForce(new Vector3(0, 0, 1));
+		Camera.main.transform.position = airplane.transform.position + new Vector3(0, 0, -10);
 	}
 }
 
